f850317f3affebf989ed71b722a8c1129275870c
Bojan Jevdjic <132005199+Bojan990@users.noreply.github.com>
2023-07-03 17:55:11 +0200

    Add files via upload

---
+++ Engine/HealingPotion.cs
@@ -0,0 +1,15 @@
+﻿namespace Engine
+{
+    public class HealingPotion : Item
+    {
+        public int AmountToHeal { get; set; }  // Propertis
+
+        public HealingPotion(int id, string name, string namePlural, int amountToHeal, int price) : base(id, name, namePlural, price)  // Constructor from Items
+        {
+            AmountToHeal = amountToHeal;
+        }
+
+    }
+
+   
+}
---
+++ Engine/Item.cs
@@ -0,0 +1,19 @@
+﻿namespace Engine
+{
+    public class Item
+    {
+        public int ID { get; set; }  // Propertis
+        public string Name { get; set; }
+        public string NamePlural { get; set; }
+        public int Price { get; set; }  
+
+        public Item(int id, string name, string namePlural, int price)  // Constructor
+        {
+            ID = id;
+            Name = name;
+            NamePlural = namePlural;
+            Price = price;
+        }
+
+    }
+}
